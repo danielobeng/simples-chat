d6a362449c1d7c3bf3085b0f8e1d21098e6be9f5
ambigit <danieldyaobeng+github_personal@gmail.com>
2018-09-02 17:09:12 +0100

    removed .env for push

--- simples_chat/src/app/init_database.js
+++ simples_chat/src/app/init_database.js
@@ -2,22 +2,22 @@ import firebase from 'firebase';
 
 // require('dotenv').config();
 
-// const config = {
-//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-//   databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
-//   storageBucket: "",
-//   messagingSenderId: process.env.REACT_APP_FIREBASE_API_ID
-// };
-
 const config = {
-apiKey: "AIzaSyDz8qm4Ulvdbnd_8KiU47EHXTyNHK_Yz2o",
-authDomain: "simples-chat.firebaseapp.com",
-databaseURL: "https://simples-chat.firebaseio.com",
-projectId: "simples-chat",
-storageBucket: "simples-chat.appspot.com",
-messagingSenderId: "1000527550418"
-      };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_STRG_BCKT,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_API_ID
+};
+
+// const config = {
+// apiKey: "AIzaSyDz8qm4Ulvdbnd_8KiU47EHXTyNHK_Yz2o",
+// authDomain: "simples-chat.firebaseapp.com",
+// databaseURL: "https://simples-chat.firebaseio.com",
+// projectId: "simples-chat",
+// storageBucket: "simples-chat.appspot.com",
+// messagingSenderId: "1000527550418"
+//       };
 
 firebase.initializeApp(config);
 
